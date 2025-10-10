69f0aea67823ff42a85404cbfe0d894a4c9aad2c
1-Thomas <10628936@bradfordcollege.ac.uk>
2025-10-10 12:55:40 +0100

    local storage

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+document.getElementById("saveBtn1").addEventListener("click", function () {
+  const steps = document.getElementById("steps").value;
+  localStorage.setItem("steps", steps);
+  alert("Steps saved successfully!");
+});
+
+
+document.getElementById("saveBtn2").addEventListener("click", function () {
+  const calories = document.getElementById("calories").value;
+  localStorage.setItem("calories", calories);
+  alert("Calories saved successfully!");
+});
+
+
+document.getElementById("saveBtn3").addEventListener("click", function () {
+  const water = document.getElementById("water").value;
+  localStorage.setItem("water", water);
+  alert("Water intake saved successfully!");
+});
+
+
+
+
 document.getElementById("activityForm").addEventListener("submit")
 const activityType = document.getElementById("activityType").value;
 const duration = document.getElementById("duration").value;
@@ -8,3 +31,5 @@ const activityData = {
     duration,
     notes,
 };
+
+
